d622c33a96269afddc92c230dec5b4c042a4cf21
kimuche <angela_id@naver.com>
2024-10-17 11:10:08 +0900

    [feat] first commit

---
+++ src/test/java/com/example/aniwhere/AniwhereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aniwhere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniwhereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
