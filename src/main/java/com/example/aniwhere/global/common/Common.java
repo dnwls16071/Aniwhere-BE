ff0bfa8d1dd77ef5d9aa886b8f2054f646082913
jwj <dnwls16071@naver.com>
2024-10-28 20:52:42 +0900

    [feat] 엔티티 공통 사항 분리

---
+++ src/main/java/com/example/aniwhere/global/common/Common.java
@@ -0,0 +1,27 @@
+package com.example.aniwhere.global.common;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class Common {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@CreatedDate
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+}
