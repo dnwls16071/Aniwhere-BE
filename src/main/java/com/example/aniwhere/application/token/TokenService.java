437a7d5d3ef8215832a3b69d569cf74c1f2b9284
jwj <dnwls16071@naver.com>
2024-10-28 21:00:51 +0900

    [feat] 액세스 토큰 재발급 서비스 클래스 추가

---
+++ src/main/java/com/example/aniwhere/application/token/TokenService.java
@@ -0,0 +1,40 @@
+package com.example.aniwhere.application.token;
+
+import com.example.aniwhere.domain.user.User;
+import com.example.aniwhere.global.error.ErrorCode;
+import com.example.aniwhere.global.error.exception.NotFoundRefreshTokenException;
+import com.example.aniwhere.infrastructure.jwt.RefreshTokenService;
+import com.example.aniwhere.infrastructure.jwt.TokenProvider;
+import com.example.aniwhere.application.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class TokenService {
+
+	private final TokenProvider tokenProvider;
+	private final RefreshTokenService refreshTokenService;
+	private final RedisTemplate<String, String> redisTemplate;
+	private final UserService userService;
+
+	public String createNewAccessToken(String refreshToken) {
+		if (!tokenProvider.validateToken(refreshToken)) {
+			throw new IllegalArgumentException("토큰이 유효하지 않습니다.");
+		}
+
+		String email = tokenProvider.getEmail(refreshToken);
+		String redisKey = "RT:" + email;
+		String redisRefreshToken = redisTemplate.opsForValue().get(redisKey);
+
+		if (redisRefreshToken == null || !redisRefreshToken.equals(refreshToken)) {
+			throw new NotFoundRefreshTokenException("해당 리프레시 토큰을 찾을 수 없습니다.", ErrorCode.NOT_FOUND_REFRESH_TOKEN);
+		}
+
+		Long userId = refreshTokenService.findByRefreshToken(refreshToken).getUserId();
+		User user = userService.findById(userId);
+
+		return tokenProvider.generateAccessToken(user);
+	}
+}
