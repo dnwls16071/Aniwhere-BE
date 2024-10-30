6fe8d2f0fe0832f757f0ec75155ed59b5bedc3a6
jwj <dnwls16071@naver.com>
2024-10-30 19:15:17 +0900

    [remove] DTO에서 사용되지 않는 필드 제거

--- src/main/java/com/example/aniwhere/domain/user/dto/UserDTO.java
+++ src/main/java/com/example/aniwhere/domain/user/dto/UserDTO.java
@@ -83,7 +83,6 @@ public class UserDTO {
 		private Long id;
 		private String email;		// 이메일
 		private String nickname;	// 별명
-		private String name;		// 이름
 		private String gender;		// 성별 ex) male, female
 		private String birthday;	// 월일 ex) 0101
 		private String birthyear;	// 생년 ex) 2000
