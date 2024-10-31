89a91efc9ce2c1950287ea04523fae4b85b0e39c
jwj <dnwls16071@naver.com>
2024-10-31 16:59:19 +0900

    [fix] Spring Security 설정 파일 수정

--- src/main/java/com/example/aniwhere/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/example/aniwhere/infrastructure/config/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.example.aniwhere.infrastructure.config;
 
+import com.example.aniwhere.application.user.MyUserDetailsService;
+import com.example.aniwhere.application.user.PrincipalOAuthDetailsService;
 import com.example.aniwhere.infrastructure.jwt.JwtTokenFilter;
 import com.example.aniwhere.infrastructure.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -16,45 +21,55 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
 @Slf4j
 public class SecurityConfig {
 
 	private final TokenProvider tokenProvider;
-
-	public SecurityConfig(TokenProvider tokenProvider) {
-		this.tokenProvider = tokenProvider;
-	}
+	private final PrincipalOAuthDetailsService principalOAuthDetailsService;
+	private final CustomOAuth2SuccessHandler customOAuth2SuccessHandler;
+	private final MyUserDetailsService myUserDetailsService;
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		return http
 				.csrf(AbstractHttpConfigurer::disable)
-				.cors(AbstractHttpConfigurer::disable)
 				.formLogin(AbstractHttpConfigurer::disable)
 				.httpBasic(AbstractHttpConfigurer::disable)
+				.sessionManagement(c ->
+						c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(auth -> auth
 						.requestMatchers(
+							 new AntPathRequestMatcher("/oauth2/authorization/kakao"),
+							 new AntPathRequestMatcher("/api/v1/oauth2/**"),
+							 new AntPathRequestMatcher("/login/**"),
+							 new AntPathRequestMatcher("/api/login"),
 							 new AntPathRequestMatcher("/api/token"),
 							 new AntPathRequestMatcher("/api/auth/login"),
                              new AntPathRequestMatcher("/api/auth/signup"),
-							 new AntPathRequestMatcher("/api/auth/kakao/callback"),
-							 new AntPathRequestMatcher("/oauth/**"),
-							 new AntPathRequestMatcher("/swagger-ui/**"),	    // swagger 문서 1
-							 new AntPathRequestMatcher("/v3/api-docs/**"),      // swagger 문서 2
+ 							 new AntPathRequestMatcher("/swagger-ui/**"),
+							 new AntPathRequestMatcher("/v3/api-docs/**"),
 							 new AntPathRequestMatcher("/")
 						).permitAll()
 						.anyRequest().authenticated()
 				)
-				.sessionManagement(session -> session
-						.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.oauth2Login(oauth2 -> oauth2
+						.userInfoEndpoint(userInfo -> userInfo
+								.userService(principalOAuthDetailsService))
+						.successHandler(customOAuth2SuccessHandler))
 				.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
 				.build();
 	}
 
 	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+
+	@Bean
 	public JwtTokenFilter jwtAuthenticationFilter() {
-		return new JwtTokenFilter(tokenProvider);
+		return new JwtTokenFilter(tokenProvider, myUserDetailsService);
 	}
 
 	@Bean
