a8e33d70d36fcd452a9aaf4c03b9b024b9e08174
jwj <dnwls16071@naver.com>
2024-10-31 16:53:19 +0900

    [fix] 동의항목으로 포함된 필드들로 수정한 인터페이스 및 해당 인터페이스를 구현한 구현체 클래스 수정

--- src/main/java/com/example/aniwhere/infrastructure/oauth2/KakaoUserInfo.java
+++ src/main/java/com/example/aniwhere/infrastructure/oauth2/KakaoUserInfo.java
@@ -1,10 +1,8 @@
 package com.example.aniwhere.infrastructure.oauth2;
 
-import com.example.aniwhere.domain.user.Sex;
-
 import java.util.Map;
 
-public class KakaoUserInfo implements ExtendedKakaoOAuth2UserInfo {
+public class KakaoUserInfo implements OAuth2UserInfo {
 
 	private Map<String, Object> attributes;
 	private Map<String, Object> attributesAccount;
@@ -17,28 +15,37 @@ public class KakaoUserInfo implements ExtendedKakaoOAuth2UserInfo {
 	}
 
 	@Override
-	public String getBirthYear() {
-		return attributesAccount.get("birthyear").toString();
+	public String getNickname() {
+		return attributesProfile.get("nickname").toString();
 	}
 
 	@Override
-	public String getBirthDay() {
-		return attributesAccount.get("birthday").toString();
+	public String getProviderId() {
+		return attributes.get("id").toString();
 	}
 
 	@Override
-	public Sex getGender() {
-		String gender = attributesAccount.get("gender").toString();
-		return Sex.valueOf(gender);
+	public String getProvider() {
+		return "kakao";
 	}
 
 	@Override
 	public String getEmail() {
-		return (String) attributesAccount.get("email");
+		return attributesAccount.get("email").toString();
 	}
 
 	@Override
-	public String getName() {
-		return (String) attributesProfile.get("nickname");
+	public String getBirthyear() {
+		return attributesAccount.get("birthyear").toString();
+	}
+
+	@Override
+	public String getBirthday() {
+		return attributesAccount.get("birthday").toString();
+	}
+
+	@Override
+	public String getSex() {
+		return attributesAccount.get("gender").toString();
 	}
 }
--- src/main/java/com/example/aniwhere/infrastructure/oauth2/OAuth2UserInfo.java
+++ src/main/java/com/example/aniwhere/infrastructure/oauth2/OAuth2UserInfo.java
@@ -2,6 +2,11 @@ package com.example.aniwhere.infrastructure.oauth2;
 
 public interface OAuth2UserInfo {
 
-	String getEmail();
-	String getName();
+	String getNickname();	// 닉네임
+	String getProviderId();	// 소셜 로그인 식별 값
+	String getProvider();	// 제공자
+	String getEmail();		// 이메일
+	String getBirthyear();  // 출생년도
+	String getBirthday();   // 출생월일
+	String getSex();        // 성별
 }
