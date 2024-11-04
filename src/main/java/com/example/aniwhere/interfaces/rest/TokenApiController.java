6266f7d6ef795ef41a773cf2e3b4433c2e76acaa
jwj <dnwls16071@naver.com>
2024-11-04 19:42:19 +0900

    [refactor] 토큰 재발급 컨트롤러 리팩토링

--- src/main/java/com/example/aniwhere/interfaces/rest/TokenApiController.java
+++ src/main/java/com/example/aniwhere/interfaces/rest/TokenApiController.java
@@ -1,16 +1,14 @@
 package com.example.aniwhere.interfaces.rest;
 
-import com.example.aniwhere.domain.token.dto.NewAccessTokenRequest;
 import com.example.aniwhere.domain.token.dto.NewAccessTokenResponse;
 import com.example.aniwhere.application.token.TokenService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
@@ -19,10 +17,11 @@ public class TokenApiController {
 	private final TokenService tokenService;
 
 	@ApiOperation("만료된 액세스 토큰을 재발급받기 위한 API")
-	@PostMapping("/token")
-	public ResponseEntity<NewAccessTokenResponse> createNewAccessToken(@RequestBody NewAccessTokenRequest request) {
+	@PostMapping("/reissue")
+	public ResponseEntity<NewAccessTokenResponse> createNewAccessToken(@RequestHeader("Refresh-Token") String refreshToken) {
+		log.info("토큰 재발급 요청");
 
-		String newAccessToken = tokenService.createNewAccessToken(request.getRefreshToken());
-		return ResponseEntity.ok(new NewAccessTokenResponse(newAccessToken));
+		String newAccessToken = tokenService.createNewAccessToken(refreshToken);
+		return ResponseEntity.ok(new NewAccessTokenResponse(newAccessToken, "토큰 재발급에 성공하셨습니다."));
 	}
 }
