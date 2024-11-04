5589e401a3100a24eb9674bc4cb7c0cffbd30a8f
jwj <dnwls16071@naver.com>
2024-11-04 19:46:38 +0900

    [style] 주석 제거 및 API 디버그 로깅 추가

--- src/main/java/com/example/aniwhere/interfaces/rest/UserApiController.java
+++ src/main/java/com/example/aniwhere/interfaces/rest/UserApiController.java
@@ -14,27 +14,29 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
-@Slf4j
 public class UserApiController {
 
 	private final UserService userService;
 	private final TokenProvider tokenProvider;
 
-	@ApiOperation("Aniwhere 서비스 회원가입")
+	@ApiOperation("Aniwhere 자체 서비스 회원가입 REST API")
 	@PostMapping("/auth/signup")
 	public ResponseEntity<UserDTO.UserSignUpResponse> signup(@Valid @RequestBody UserDTO.UserSignUpRequest request) {
 		log.info("회원가입 요청");
+
 		User user = userService.signup(request);
 		return ResponseEntity.ok(new UserDTO.UserSignUpResponse(user));
 	}
 
-	@ApiOperation("Aniwhere 서비스 로그인")
+	@ApiOperation("Aniwhere 자체 서비스 로그인 REST API")
 	@PostMapping("/auth/login")
 	public ResponseEntity<Void> login(@Valid @RequestBody UserDTO.UserSignInRequest request) {
 		log.info("로그인 요청");
+
 		JwtToken jwtToken = userService.signin(request);
 
 		HttpHeaders headers = new HttpHeaders();
@@ -46,16 +48,14 @@ public class UserApiController {
 				.build();
 	}
 
-	@ApiOperation("Aniwhere 서비스 로그아웃")
+	@ApiOperation("Aniwhere 자체 서비스 로그아웃 REST API")
 	@PostMapping("/auth/logout")
 	public ResponseEntity<String> logout(HttpServletRequest request) {
 		log.info("로그아웃 요청");
 
-		// Access Token과 Refresh Token을 파싱
 		JwtToken jwtToken = tokenProvider.resolveToken(request);
-
-		// 레디스의 블랙 리스트에 저장
 		userService.logout(jwtToken);
+
 		return ResponseEntity.ok().body("성공적으로 로그아웃되었습니다.");
 	}
 }
